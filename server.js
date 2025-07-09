0f84b86fbf6898e7e3553d079b0aa744848ace1a
Bhumika2906 <bhumikabhardwaj.2906@gmail.com>
2025-07-09 12:06:52 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
@@ -14,7 +15,10 @@ app.use(cors({
   credentials: true
 }));
 
-mongoose.connect(process.env.MONGODB_URI)
+mongoose.connect(process.env.MONGODB_URI , {
+    useNewUrlParser: true,
+  useUnifiedTopology: true
+})
 .then(() => {
     console.log('Connected to MongoDB successfully!')
 })
